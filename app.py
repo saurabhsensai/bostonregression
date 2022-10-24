85392938f6c29493455c10cb53f2a2924cf68937
saurabhsensai <saurabhnale9@gmail.com>
2022-10-24 09:39:21 +0530

    hii saurabh

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from crypt import methods
+
 from distutils.log import debug
 import pickle
 from typing import final
 from unicodedata import name  
-from flask import flask, request,app,jsonify,url_for,render_template
+from flask import Flask, request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd 
 
 
 app = Flask(__name__)
 #load the model
-regmodel = pickle.load(open("regmpdel.pkl",'rb'))
+regmodel = pickle.load(open("regmodel.pkl",'rb'))
 scalar = pickle.load(open("scaling.pkl",'rb'))
 
 
@@ -28,6 +28,15 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
 
 @app.route('/predict',methods=['POST'])
 def predict():
